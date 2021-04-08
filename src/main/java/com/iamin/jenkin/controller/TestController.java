bcd63135a05f2431486c6d743736f1453de59f8e
Amol Dadas <amol@iamin.tech>
2021-04-08 19:21:56 +0530

    test jenkin first commit

---
+++ src/main/java/com/iamin/jenkin/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.iamin.jenkin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping
+    public String hareKrishna(){
+        return "Hare Krishna";
+    }
+}
